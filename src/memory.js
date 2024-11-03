d06340ffdf2d99eb2d416286c6c6b0a6ab1e4ad0
François <76422000+Francois-lqc@users.noreply.github.com>
2024-11-03 12:22:20 +0100

    Update memory.js imports

--- src/memory.js
+++ src/memory.js
@@ -3,18 +3,7 @@
 // Import only what you need, to help your bundler optimize final code size using tree shaking
 // see https://developer.mozilla.org/en-US/docs/Glossary/Tree_shaking)
 
-import {
-  Color,
-  PerspectiveCamera,
-  Scene,
-  Vector2,
-  WebGLRenderer,
-  PMREMGenerator,
-  Raycaster,
-  BoxGeometry,
-  MeshStandardMaterial,
-  Mesh
-} as THREE from './three';
+import * as THREE from './three';
 
 import { initializeMain } from './main.js';
 
